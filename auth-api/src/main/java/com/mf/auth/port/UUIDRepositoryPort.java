6ce78f3c45466f79c2a7be8bb7e30f3827ad6b35
Alexander Tolpeko <alextolpeko@gmail.com>
2024-07-27 18:00:39 +0300

    AuthAPI: UUID repository interface

---
+++ auth-api/src/main/java/com/mf/auth/port/UUIDRepositoryPort.java
@@ -0,0 +1,34 @@
+package com.mf.auth.port;
+
+import com.mf.auth.domain.entity.Token;
+import com.mf.auth.port.exception.DataAccessException;
+import com.mf.auth.port.exception.DataModificationException;
+
+import java.util.Optional;
+
+/**
+ * Interface for managing UUIDs in a repository.
+ */
+public interface UUIDRepositoryPort {
+
+    /**
+     * Find a UUID by its value.
+     *
+     * @param value  UUID value
+     *
+     * @return found UUID or Optional.empty() if none found
+     *
+     * @throws DataAccessException in repository is unavailable
+     */
+    Optional<Token> findByValue(String value);
+
+    /**
+     * Save the specified UUID.
+     *
+     * @param uuid  UUID to save
+     *
+     * @throws DataModificationException  if a UUID cannot be saved
+     * @throws DataAccessException        in repository is unavailable
+     */
+    void save(Token uuid);
+}
