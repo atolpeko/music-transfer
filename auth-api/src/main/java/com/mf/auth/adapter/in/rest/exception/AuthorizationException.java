f447698b536575bb2cf4007ae7ad2e6062ce31f7
Alexander Tolpeko <alextolpeko@gmail.com>
2024-07-28 12:26:06 +0300

    AuthAPI: exception interceptor

---
+++ auth-api/src/main/java/com/mf/auth/adapter/in/rest/exception/AuthorizationException.java
@@ -0,0 +1,24 @@
+package com.mf.auth.adapter.in.rest.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class AuthorizationException extends RuntimeException {
+    private final HttpStatus status;
+
+    public AuthorizationException(HttpStatus status) {
+        super();
+        this.status = status;
+    }
+
+    public AuthorizationException(HttpStatus status, String message) {
+        super(message);
+        this.status = status;
+    }
+
+    public AuthorizationException(HttpStatus status, String message, Throwable cause) {
+        super(message, cause);
+        this.status = status;
+    }
+}
