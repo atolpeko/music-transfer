bcbfca9ec6c56dd2ac2ccd2da94e0d2cb247000f
Alexander Tolpeko <alextolpeko@gmail.com>
2024-07-28 12:27:45 +0300

    AuthAPI: token API implementation

---
+++ auth-api/src/main/java/com/mf/auth/adapter/in/rest/controller/TokenController.java
@@ -0,0 +1,32 @@
+package com.mf.auth.adapter.in.rest.controller;
+
+import com.mf.auth.adapter.in.rest.api.TokenAPI;
+import com.mf.auth.adapter.in.rest.entity.JWTRestEntity;
+import com.mf.auth.adapter.in.rest.mapper.JWTMapper;
+import com.mf.auth.usecase.JWTUseCase;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class TokenController implements TokenAPI {
+	private final JWTUseCase useCase;
+	private final JWTMapper jwtMapper;
+
+	@Override
+	public JWTRestEntity getJwt(String accessToken) {
+		var jwt = useCase.obtain(accessToken);
+		return jwtMapper.toRestEntity(jwt);
+	}
+
+	@Override
+	public ResponseEntity<String> validateJwt(String jwt) {
+		var valid = useCase.isValid(jwt);
+		return valid
+			? ResponseEntity.ok("valid")
+			: ResponseEntity.status(401).body("not valid");
+	}
+}
---
+++ auth-api/src/test/java/com/mf/auth/fixture/TokenFixture.java
@@ -0,0 +1,6 @@
+package com.mf.auth.fixture;
+
+public class TokenFixture {
+    public static final String GET_JWT_URL = "/api/auth/token/get";
+    public static final String VALIDATE_URL = "/api/auth/token/validate";
+}
