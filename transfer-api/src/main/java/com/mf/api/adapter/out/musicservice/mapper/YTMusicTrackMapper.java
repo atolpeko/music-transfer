78b9bcbfa63f52be7e552d25533b4627b606794a
atolpeko <alextolpeko@gmail.com>
2024-08-14 00:30:43 +0300

    TransferAPI: improve youtube music track mapping

--- transfer-api/src/main/java/com/mf/api/adapter/out/musicservice/mapper/YTMusicTrackMapper.java
+++ transfer-api/src/main/java/com/mf/api/adapter/out/musicservice/mapper/YTMusicTrackMapper.java
@@ -9,20 +9,40 @@ import java.util.Optional;
 public class YTMusicTrackMapper {
 
 	public Track map(LinkedHashMap<Object, Object> restResponse) {
-		var data = (LinkedHashMap<?, ?>) restResponse.get("snippet");
-		var artist = Optional.ofNullable((String) data.get("videoOwnerChannelTitle"))
-			.orElse((String) data.get("channelTitle"))
-			.replace(" - topic", "");
-
+		var data = (LinkedHashMap<Object, Object>) restResponse.get("snippet");
+		var title = (String) data.get("title");
+		var	artist = extractArtist(data);
 		return Track.builder()
 			.id((String) restResponse.get("id"))
 			.name((String) data.get("title"))
+			.albumName(extractAlbum(data, artist, title))
 			.artists(List.of(artist))
 			.build();
 	}
 
+	private String extractArtist(LinkedHashMap<Object, Object> data) {
+		return Optional.ofNullable((String) data.get("videoOwnerChannelTitle"))
+			.orElse((String) data.get("channelTitle"))
+			.replace(" - Topic", "");
+	}
+
+	private String extractAlbum(
+		LinkedHashMap<Object, Object> data,
+		String artist,
+		String title
+	) {
+		try {
+			var desc = (String) data.get("description");
+			var titleArtist = title + " · " + artist + "\n\n";
+			var artistEnd = desc.substring(desc.indexOf(titleArtist) + titleArtist.length());
+			return artistEnd.substring(0, artistEnd.indexOf("\n"));
+		} catch (Exception e) {
+			return "";
+		}
+	}
+
 	public List<Track> mapList(LinkedHashMap<Object, Object> restResponse) {
-		var tracks = (List<LinkedHashMap>) restResponse.get("items");
+		var tracks = (List<LinkedHashMap<Object, Object>>) restResponse.get("items");
 		return tracks.stream()
 			.map(this::map)
 			.toList();
