7b7e6cf9f20b31452d3146b61a68527014198d36
atolpeko <alextolpeko@gmail.com>
2024-08-13 15:46:34 +0300

    TransferAPI: rest template configuration

---
+++ transfer-api/src/main/java/com/mf/api/boot/config/web/RestTemplateConfig.java
@@ -0,0 +1,24 @@
+package com.mf.api.boot.config.web;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestTemplateConfig {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		var converter = new MappingJackson2HttpMessageConverter();
+		converter.setSupportedMediaTypes(
+			Collections.singletonList(MediaType.APPLICATION_FORM_URLENCODED));
+
+		var template = new RestTemplate();
+		template.getMessageConverters().add(converter);
+		return template;
+	}
+}
