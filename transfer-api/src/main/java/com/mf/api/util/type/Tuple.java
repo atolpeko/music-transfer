a6f3388d129efa380d3dbe1392fbaa235f22129d
atolpeko <alextolpeko@gmail.com>
2024-08-15 10:45:02 +0300

    TransferAPI: tuple build method

--- transfer-api/src/main/java/com/mf/api/util/type/Tuple.java
+++ transfer-api/src/main/java/com/mf/api/util/type/Tuple.java
@@ -7,4 +7,8 @@ public class Tuple <T, K> {
 
 	private final T first;
 	private final K second;
+
+	public static <T, K> Tuple<T, K> of(T first, K second) {
+		return new Tuple<>(first, second);
+	}
 }
