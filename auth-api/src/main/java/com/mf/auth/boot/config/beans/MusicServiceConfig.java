7ca32ccd26403d1cd9533fccab5782e0365700cc
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-22 21:35:53 +0300

    AuthService: removed circuit breaker

--- auth-api/src/main/java/com/mf/auth/boot/config/beans/MusicServiceConfig.java
+++ auth-api/src/main/java/com/mf/auth/boot/config/beans/MusicServiceConfig.java
@@ -5,8 +5,6 @@ import com.mf.auth.adapter.out.musicservice.YTMusicAdapter;
 import com.mf.auth.adapter.properties.MusicServiceProperties;
 import com.mf.auth.port.MusicServicePort;
 
-import io.github.resilience4j.circuitbreaker.CircuitBreaker;
-
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,18 +16,16 @@ public class MusicServiceConfig {
 	@Bean
 	public MusicServicePort spotifyAdapter(
 		@Qualifier("spotifyProperties") MusicServiceProperties properties,
-		RestTemplate restTemplate,
-		CircuitBreaker breaker
+		RestTemplate restTemplate
 	) {
-		return new SpotifyAdapter(properties, restTemplate, breaker);
+		return new SpotifyAdapter(properties, restTemplate);
 	}
 
 	@Bean
 	public MusicServicePort ytMusicAdapter(
 		@Qualifier("ytMusicProperties") MusicServiceProperties properties,
-		RestTemplate restTemplate,
-		CircuitBreaker breaker
+		RestTemplate restTemplate
 	) {
-		return new YTMusicAdapter(properties, restTemplate, breaker);
+		return new YTMusicAdapter(properties, restTemplate);
 	}
 }
