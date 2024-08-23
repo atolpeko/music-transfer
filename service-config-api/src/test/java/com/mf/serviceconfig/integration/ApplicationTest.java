4e514e926fdd3891f700ea34c800991512aee4d4
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-23 15:29:17 +0300

    ServiceConfigApi: context load test

---
+++ service-config-api/src/test/java/com/mf/serviceconfig/integration/ApplicationTest.java
@@ -0,0 +1,16 @@
+package com.mf.serviceconfig.integration;
+
+import com.mf.serviceconfig.config.IntegrationTest;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+@IntegrationTest
+@Tag("category.IntegrationTest")
+class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
