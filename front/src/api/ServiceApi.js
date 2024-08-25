315b46fca8a5cf128df9daa60628093966873b85
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-25 23:40:51 +0300

    Front: service API

---
+++ front/src/api/ServiceApi.js
@@ -0,0 +1,6 @@
+export const fetchServices = async () => {
+	let url = window.DOMAIN + window.SERVICE_API + '/services';
+  console.log(`Fetching services from ${url}`)
+  return fetch(url, { method: 'GET' })
+		.then(response => response.json())
+}
