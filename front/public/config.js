aa9646251261addc1bf0d904dee9d108bb52fe44
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-26 19:51:31 +0300

    TransferService: transfer API

--- front/public/config.js
+++ front/public/config.js
@@ -1,3 +1,4 @@
 window.DOMAIN = 'http://localhost';
 window.SERVICE_API = ':6010/api';
 window.AUTH_API = ':5980/api/auth';
+window.TRANSFER_API = ':5990/api/transfer';
--- front/src/api/TransferApi.js
+++ front/src/api/TransferApi.js
@@ -1,136 +1,38 @@
-export const fetchTracks = async () => {
-	return [
-		{
-			'id': '2434343',
-			'name': 'Track 1',
-			'albumName': 'Album 1',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 1'
-			]
-		},
-		{
-			'id': 't453435',
-			'name': 'Track 2',
-			'albumName': 'Album 2',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 2'
-			]
-		},
-		{
-			'id': '45gr453',
-			'name': 'Track 3',
-			'albumName': 'Album 3',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 3'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		},
-		{
-			'id': 'kdfjf',
-			'name': 'Track 4',
-			'albumName': 'Album 4',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'artists': [
-				'Artist 4'
-			]
-		}
-	]
+export const fetchTracks = async (service, authToken) => {
+	const url = `${window.DOMAIN}${window.TRANSFER_API}/tracks?service=${service}`;
+	return fetchAll(url, authToken, 'tracks');
 }
 
-export const fetchPlaylists = async () => {
-	return [
-		{
-			'id': '434w24',
-			'name': 'Playlist 1',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'tracks': [
-				{
-					'id': '2434343',
-					'name': 'Track 1',
-					'albumName': 'Album 1',
-					'artists': [
-						'Artist 1'
-					]
-				},
-				{
-					'id': 't453435',
-					'name': 'Track 2',
-					'albumName': 'Album 2',
-					'artists': [
-						'Artist 2'
-					]
-				}
-			]
-		},
-		{
-			'id': 'dfgrtre34',
-			'name': 'Playlist 2',
-			'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Spotify_icon.svg/1024px-Spotify_icon.svg.png?20220821125323',
-			'tracks': [
-				{
-					'id': '45gr453',
-					'name': 'Track 3',
-					'albumName': 'Album 3',
-					'artists': [
-						'Artist 3'
-					]
-				},
-				{
-					'id': 'kdfjf',
-					'name': 'Track 4',
-					'albumName': 'Album 4',
-					'artists': [
-						'Artist 4'
-					]
-				}
-			]
-		}
-	]
+export const fetchAll = async (baseUrl, authToken, entity) => {
+	let all = [];
+	let url = baseUrl;
+	while (url) {
+		console.log(`Fetching ${entity} from ${url}`);
+		const response = await fetch(url, { 
+			method: 'GET',
+			headers: {Authorization: `Bearer ${authToken}`} 
+		}).then(res => res.json());
+
+		all = all.concat(response.items);
+		url = (response.next) 
+			? `${baseUrl}&next=${response.next}`
+			: null;
+	}
+
+	console.log(all);
+	return all;
+}
+
+export const fetchPlaylists = async (service, authToken) => {
+	const url = `${window.DOMAIN}${window.TRANSFER_API}/playlists?service=${service}`;
+	const playlists = await fetchAll(url, authToken, 'playlists');
+	playlists.forEach(playlist =>
+		 playlist.tracks = fetchPlaylistTracks(service, authToken, playlist.id));
+
+}
+
+export const fetchPlaylistTracks = async (service, authToken, playlistId) => {
+	const endpoint = `${window.DOMAIN}${window.TRANSFER_API}/tracks`;
+	const url = `${endpoint}?service=${service}&playlistId=${playlistId}`;
+	return fetchAll(url, authToken, 'playlist tracks');
 }
