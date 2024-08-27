8cad2f5bd588b13b036bd93a485bdb0bd449c796
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-27 15:50:38 +0300

    Front: fix transfer setup page loading

--- front/src/components/transfer/TransferSetupPage.js
+++ front/src/components/transfer/TransferSetupPage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import ClipLoader from "react-spinners/ClipLoader";
+import Spinner from '../spinner/Spinner';
 import SelectableCard from '../SelectableCard';
 
 import './TransferPage.css';
@@ -8,7 +8,8 @@ const TransferSetupPage = ({ source, target, loadTracks, loadPlaylists, onTransf
 
   const [tracks, setTracks] = useState([]);
   const [playlists, setPlaylists] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [tracksLoading, setTracksLoading] = useState(true);
+  const [playlistsLoading, setPlaylistsLoading] = useState(true);
   const [entity, setEntity] = useState('Tracks');    
   const [showModal, setShowModal] = useState(false);
   const [error, setError] = useState('');
@@ -17,19 +18,28 @@ const TransferSetupPage = ({ source, target, loadTracks, loadPlaylists, onTransf
     loadTracks().then(data => {
       data.forEach(track => track.selected = true)
       setTracks(data);
+      setTracksLoading(false);
     }).catch(error => {
-      setError(`Failed to load tracks`)
-      setLoading(false);
+      setError(`Failed to load tracks: ${error}`)
+      setTracksLoading(false);
     });
     
     loadPlaylists().then(data => {
       data.forEach(playlist => playlist.selected = true)
       setPlaylists(data);
-      setLoading(false);
+      setPlaylistsLoading(false);
     }).catch(error => {
-      setError(`Failed to load playlists`)
-      setLoading(false);
+      setError(`Failed to load playlists: ${error}`)
+      setPlaylistsLoading(false);
     });
+  }, []);
+
+  useEffect(() => {
+    window.onclick = event => {
+      const modal = document.getElementById('transferModal'); 
+      if (modal && event.target.id == modal.id)
+        setShowModal(false);
+    };
   }, []);
 
   const toggleModal = () => setShowModal(!showModal);
@@ -99,76 +109,69 @@ const TransferSetupPage = ({ source, target, loadTracks, loadPlaylists, onTransf
           </h1>
         </div>
         <div className="row justify-content-center section">
-          <div className="card transfer-info-card align-items-center justify-content-center
-            shadow-sm p-6 mb-5 bg-white rounded">
-            <h4 className="section">Found {tracks.length} Liked Tracks
-              and {playlists.length} Playlists</h4> 
-            <div className="row align-items-center justify-content-center section">
-              <button className="transfer-page-button"
-                      onClick={handleShowTracksClick}>
-                View Tracks
-              </button>
-              <button className="transfer-page-button"
-                      onClick={handleShowPlaylistsClick}>
-                View Playlists
-              </button>
-              <button className="transfer-page-button transfer-page-run-button"
-                      onClick={handleTransferClick}>
-                Transfer
-              </button>
-            </div>
-            <div className="row justify-content-center error-section"> 
-              { error && <p style= {{ color: 'red' }}> {error} </p> }
-            </div>    
-
-            { showModal && (
-              <div className="modal transfer-modal d-block" role="dialog">
-                <div className="modal-dialog modal-lg" role="document">
-                  <div className="modal-content">
-                    <div className="modal-header justify-content-center">
-                      <h4 className="modal-title">Select {entity} to Transfer</h4>
-                    </div>
-                    <div className="modal-body">
-                      <div className="row align-items-center justify-content-center">
-                        { entity == 'Tracks' 
-                          ? renderTracks() 
-                          : renderPlaylists() 
-                        }
-                      </div>
-                    </div>
-                    <div className="modal-footer">
-                      <button type="button" 
-                              onClick={toggleModal}>
-                        Close
-                      </button>
-                    </div>
+          { error 
+            ? <p style= {{ color: 'red' }}> {error} </p>
+            : <div className="card transfer-info-card align-items-center justify-content-center
+                shadow-sm p-6 mb-5 bg-white rounded">
+                <h4 className="section">Found {tracks.length} Liked Tracks
+                  and {playlists.length} Playlists</h4> 
+                <div className="row align-items-center justify-content-center section">
+                  <button className="transfer-page-button"
+                          onClick={handleShowTracksClick}>
+                    View Tracks
+                  </button>
+                  <button className="transfer-page-button"
+                          onClick={handleShowPlaylistsClick}>
+                    View Playlists
+                  </button>
+                  <button className="transfer-page-button transfer-page-run-button"
+                          onClick={handleTransferClick}>
+                    Transfer
+                  </button>
+                </div>
+                <div className="row justify-content-center error-section"> 
+                  { error && <p style= {{ color: 'red' }}> {error} </p> }
+                </div> 
+              </div>   
+          }
+        </div>   
+        { showModal && (
+          <div id='transferModal' className="modal transfer-modal d-block" role="dialog">
+            <div className="modal-dialog modal-lg" role="document">
+              <div className="modal-content">
+                <div className="modal-header justify-content-center">
+                  <h4 className="modal-title">Select {entity} to Transfer</h4>
+                </div>
+                <div className="modal-body">
+                  <div className="row align-items-center justify-content-center">
+                    { entity == 'Tracks'  
+                      ? renderTracks() 
+                      : renderPlaylists() 
+                    }
                   </div>
                 </div>
+                <div className="modal-footer">
+                  <button type="button" 
+                          onClick={toggleModal}>
+                    Close
+                  </button>
+                </div>
               </div>
-            )}
-
-            { showModal && 
-              <div className="modal-backdrop fade show"/>
-            }
+            </div>
           </div>
-        </div>   
-      </div>
-    );
-  }
+        )}
 
-  const renderSpinner = () => {
-    return (
-      <div className="row justify-content-center section">
-        <ClipLoader loading={loading}
-                    aria-label="Loading"
-                    data-testid="loader" />
+        { showModal && <div className="modal-backdrop fade show"/> }  
       </div>
     );
   }
 
   return (
     <div className="container center-container align-items-center justify-content-center">
-      { loading ? renderSpinner() : renderContent() }
+      { tracksLoading || playlistsLoading 
+        ? <Spinner text={`Loading tracks and playlists from ${source}...`}/> 
+        : renderContent() 
+        }
     </div>
   );
 }
