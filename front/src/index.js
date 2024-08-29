2b88a44e123720f0769ca3c7e562ba5410c8e8ad
Alexander Tolpeko <alextolpeko@gmail.com>
2024-08-29 10:16:58 +0300

    Front: error boundary

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import ErrorBoundary from './ErrorBoundary';
 import App from './App';
 
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
- 
-    <App />
-
+<ErrorBoundary fallback={<p>Something went wrong</p>}>
+	<App />
+</ErrorBoundary>
 )
 
